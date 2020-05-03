745fe92508955631f500b19d75ff2a88a8f94488
Kevin Mathias <komathias1@gmail.com>
2020-05-03 02:55:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,8 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :image_url
       t.string :size
-      t.timestamp :created_at
-      t.timestamp :updated_at
+      t.timestamps :created_at
+      t.timestamps :updated_at
     end
   end
 end
